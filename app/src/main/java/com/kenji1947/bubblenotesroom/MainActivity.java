39bb935ffd30a3f9380104a1feca84449573e4a5
kenji47 <fw190a2@gmail.com>
2017-10-05 04:12:44 +0300

    Init commit

---
+++ app/src/main/java/com/kenji1947/bubblenotesroom/MainActivity.java
@@ -0,0 +1,10 @@
+package com.kenji1947.bubblenotesroom;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kenji1947 on 05.10.2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
